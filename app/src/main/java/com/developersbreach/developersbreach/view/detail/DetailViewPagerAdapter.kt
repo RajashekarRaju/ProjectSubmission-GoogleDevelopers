2585e59065c00c95a3b97943be78114d2a27e3ed
rajas <rajashekarraju123@gmail.com>
2020-05-31 11:16:28 +0530

    ViewPager2 for DetailFragment.kt to swipe between articles

---
+++ app/src/main/java/com/developersbreach/developersbreach/view/detail/DetailViewPagerAdapter.kt
@@ -0,0 +1,39 @@
+package com.developersbreach.developersbreach.view.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.developersbreach.developersbreach.databinding.ItemViewPagerBinding
+import com.developersbreach.developersbreach.model.Articles
+
+
+class DetailViewPagerAdapter :
+    ListAdapter<Articles, DetailViewPagerAdapter.DetailViewHolder>(Articles.DiffCallback) {
+
+
+    class DetailViewHolder(
+        private var binding: ItemViewPagerBinding
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            articles: Articles
+        ) {
+            binding.article = articles
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        return DetailViewHolder(
+            ItemViewPagerBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        val articles: Articles = getItem(position)
+        holder.bind(articles)
+    }
+}
