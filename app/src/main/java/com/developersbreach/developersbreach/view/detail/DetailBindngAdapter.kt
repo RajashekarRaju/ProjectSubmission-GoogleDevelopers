a12d8b55f93635ec5968b4449d1e6a32a020250d
rajas <rajashekarraju123@gmail.com>
2020-06-01 15:49:16 +0530

    Added views to DetailFragment with bindingAdapters, up navigation support

--- app/src/main/java/com/developersbreach/developersbreach/view/detail/DetailBindngAdapter.kt
+++ app/src/main/java/com/developersbreach/developersbreach/view/detail/DetailBindngAdapter.kt
@@ -1,11 +1,50 @@
 package com.developersbreach.developersbreach.view.detail
 
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.BindingAdapter
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.developersbreach.developersbreach.model.Articles
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 
-@BindingAdapter("bindDetailText")
-fun TextView.setDetailTextView(articles: Articles) {
-    this.text = articles.title
+@BindingAdapter("bindImage")
+fun ImageView.setImageResource(articles: Articles) {
+    Glide.with(this.context).load(articles.banner).into(this)
+}
+
+
+@BindingAdapter("bindToolbar")
+fun Toolbar.setToolbar(articles: Articles) {
+    let { toolBar ->
+        toolBar.title = articles.title
+        toolBar.setNavigationOnClickListener { view ->
+            Navigation.findNavController(view).navigateUp()
+        }
+    }
+}
+
+
+@BindingAdapter("bindAppbar", "collapsingToolbarView")
+fun AppBarLayout.setAppbar(articles: Articles, collapsingToolbar: CollapsingToolbarLayout) {
+    this.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+        var isShow = false
+        var scrollRange = -1
+        override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+            if (scrollRange == -1) {
+                scrollRange = appBarLayout.totalScrollRange
+            }
+            if (scrollRange + verticalOffset == 0) {
+                // Show title when completely collapsed.
+                collapsingToolbar.title = articles.title
+                isShow = true
+            } else if (isShow) {
+                // Hide title when collapsedToolBar is completely visible using empty string.
+                collapsingToolbar.title = ""
+                isShow = false
+            }
+        }
+    })
 }
