b97bdbbd8e7b9755b86f555f80d0d1b5822694be
rajas <rajashekarraju123@gmail.com>
2020-06-02 19:52:59 +0530

    Merged Tags.kt into Articles.kt properties

--- app/src/main/java/com/developersbreach/developersbreach/model/Tags.kt
+++ app/src/main/java/com/developersbreach/developersbreach/model/Tags.kt
@@ -1,6 +1,10 @@
 package com.developersbreach.developersbreach.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 class Tags(
     val id: Int,
     val name: String
-)
+) : Parcelable
