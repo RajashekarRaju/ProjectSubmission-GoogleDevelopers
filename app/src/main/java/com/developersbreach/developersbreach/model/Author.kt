58250d7fb28f7a990fb313ac8ece289a11a445b1
rajas <rajashekarraju123@gmail.com>
2020-06-02 10:49:13 +0530

    New data classes - Tags, Author for getting specific article properties

---
+++ app/src/main/java/com/developersbreach/developersbreach/model/Author.kt
@@ -0,0 +1,8 @@
+package com.developersbreach.developersbreach.model
+
+class Author(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val avatar: String
+)
---
+++ app/src/main/java/com/developersbreach/developersbreach/model/Tags.kt
@@ -0,0 +1,6 @@
+package com.developersbreach.developersbreach.model
+
+class Tags(
+    val id: Int,
+    val name: String
+)
