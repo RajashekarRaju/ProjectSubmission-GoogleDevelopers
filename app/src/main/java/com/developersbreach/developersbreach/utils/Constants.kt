3bec911c23aebc6a03e9f9b78ee5492b9ed0f2c5
rajas <rajashekarraju123@gmail.com>
2020-06-06 18:30:18 +0530

    New class constants which provides static access for all variables in other classes

---
+++ app/src/main/java/com/developersbreach/developersbreach/utils/Constants.kt
@@ -0,0 +1,62 @@
+package com.developersbreach.developersbreach.utils
+
+const val DATABASE_NAME: String = "articles.db"
+const val DATABASE_ENTITY_TABLE_ARTICLE = "articles_table"
+const val DATABASE_ENTITY_TABLE_FAVORITES = "favorites_table"
+
+const val COLUMN_ARTICLE_BASE_ID = "article_base_id"
+const val COLUMN_ARTICLE_ID = "article_id"
+const val COLUMN_ARTICLE_TITLE = "article_title"
+const val COLUMN_ARTICLE_BANNER = "article_banner"
+const val COLUMN_ARTICLE_POSTED_DATE = "article_posted_date"
+const val COLUMN_ARTICLE_URL_LINK = "article_url_link"
+const val COLUMN_ARTICLE_EXCERPT = "article_excerpt"
+const val COLUMN_ARTICLE_AUTHOR_ID = "article_author_id"
+const val COLUMN_ARTICLE_TAGS_LIST = "article_tags_list"
+
+const val COLUMN_FAVORITE_BASE_ID = "favorite_base_id"
+const val COLUMN_FAVORITE_ID = "favorite_id"
+const val COLUMN_FAVORITE_TITLE = "favorite_title"
+const val COLUMN_FAVORITE_BANNER = "favorite_banner"
+const val COLUMN_FAVORITE_POSTED_DATE = "favorite_posted_date"
+const val COLUMN_FAVORITE_URL_LINK = "favorite_url_link"
+const val COLUMN_FAVORITE_EXCERPT = "favorite_excerpt"
+const val COLUMN_FAVORITE_AUTHOR_ID = "favorite_author_id"
+const val COLUMN_FAVORITE_TAGS_LIST = "favorite_tags_list"
+
+
+const val CHECK_WITH_NULL_ASSERTION: String = "null"
+const val CHECK_WITH_EMPTY_ASSERTION: String = ""
+
+const val PASS_PREFERENCE_CONTACT_KEY: String = "Contact"
+const val PASS_PREFERENCE_DEVELOPER_KEY: String = "Developer"
+
+
+const val JSON_OBJECT_TITLE = "title"
+const val JSON_OBJECT_EXCERPT = "excerpt"
+const val JSON_OBJECT_LINKS = "_links"
+const val JSON_ARRAY_LINKS = "wp:term"
+
+const val ARTICLE_ID = "id"
+const val ARTICLE_TITLE = "rendered"
+const val ARTICLE_POSTED_DATE = "date"
+const val ARTICLE_URL_LINK = "link"
+const val ARTICLE_EXCERPT = "rendered"
+const val ARTICLE_AUTHOR = "author"
+const val ARTICLE_BANNER = "jetpack_featured_media_url"
+const val ARTICLE_TAGS = "href"
+
+const val JSON_OBJECT_AVATAR = "avatar_urls"
+const val AUTHOR_ID = "id"
+const val AUTHOR_NAME = "name"
+const val AUTHOR_DESCRIPTION = "description"
+const val AUTHOR_AVATAR = "96"
+
+const val TAG_ID = "id"
+const val TAG_NAME = "name"
+
+
+const val SCHEME_AUTHORITY = "https://developersbreach.com"
+const val APPEND_PATH = "wp-json/wp/v2"
+const val APPEND_ENDPOINT_POSTS = "posts"
+const val APPEND_ENDPOINT_USERS = "users/107376512"
