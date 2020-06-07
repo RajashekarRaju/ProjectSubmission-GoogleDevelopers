9c517906c6b423ca42c665b21681cfc295641707
rajas <rajashekarraju123@gmail.com>
2020-06-07 22:37:03 +0530

    Added CHipGroup to show Tags for selected articles, Format Date and Author titles, Added new views to layout

--- app/src/main/java/com/developersbreach/developersbreach/view/detail/DetailBindingAdapter.kt
+++ app/src/main/java/com/developersbreach/developersbreach/view/detail/DetailBindingAdapter.kt
@@ -1,5 +1,7 @@
 package com.developersbreach.developersbreach.view.detail
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -13,64 +15,78 @@ import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.developersbreach.developersbreach.R
 import com.developersbreach.developersbreach.model.Articles
+import com.developersbreach.developersbreach.model.Tags
 import com.developersbreach.developersbreach.viewModel.DetailViewModel
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
 
 @BindingAdapter("bindCollapsingBannerImage")
-fun ImageView.setImageResource(articles: Articles) {
-    Glide.with(this.context).load(articles.banner).into(this)
-}
-
-
-@BindingAdapter("bindTagsText")
-fun TextView.setTagText(articles: Articles) {
-    val tagList = articles.tagList
-    for (tag in tagList) {
-        this.text = tagList.toString()
-    }
+fun ImageView.setImageResource(
+    urlString: String
+) {
+    Glide.with(this.context).load(urlString).into(this)
 }
 
 
 @BindingAdapter("bindButtonWebView")
-fun Button.setButtonWebView(articles: Articles) {
+fun Button.setButtonWebView(
+    stringUrl: String
+) {
     this.text = this.context.getString(R.string.open_article_button_text)
     this.setOnClickListener {
         val action: NavDirections =
-            DetailFragmentDirections.DetailToWebFragment(articles.urlLink)
+            DetailFragmentDirections.DetailToWebFragment(stringUrl)
         findNavController().navigate(action)
     }
 }
 
 
-@BindingAdapter("bindAppbar", "bindCollapsingToolbarView")
-fun AppBarLayout.setAppbar(articles: Articles, collapsingToolbar: CollapsingToolbarLayout) {
-    this.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
-        var isShow = false
-        var scrollRange = -1
-        override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-            if (scrollRange == -1) {
-                scrollRange = appBarLayout.totalScrollRange
-            }
-            if (scrollRange + verticalOffset == 0) {
-                // Show title when completely collapsed.
-                collapsingToolbar.title = articles.title
-                isShow = true
-            } else if (isShow) {
-                // Hide title when collapsedToolBar is completely visible using empty string.
-                collapsingToolbar.title = ""
-                isShow = false
-            }
-        }
-    })
+@BindingAdapter("bindArticleExcerpt")
+fun TextView.setArticleExcerpt(
+    excerpt: String
+) {
+    val formatExcerpt: String = excerpt.drop(3).dropLast(5)
+    this.text = formatExcerpt
+}
+
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("bindAuthorAndData")
+fun TextView.setAuthorAndDate(
+    date: String
+) {
+    val formatDate: String = date.dropLast(9)
+    val author: String = this.context.getString(R.string.author_name)
+    this.text = "$author  |  $formatDate"
+}
+
+
+@BindingAdapter("bindChipGroup")
+fun ChipGroup.setChipGroup(
+    tagsList: List<Tags>
+) {
+    val inflater: LayoutInflater = LayoutInflater.from(this.context)
+    val chipList: List<Chip> = tagsList.map { tags ->
+        val chip = inflater.inflate(R.layout.item_action_chip, this, false) as Chip
+        chip.text = tags.name
+        chip
+    }
+
+    this.removeAllViews()
+    for (currentChip in chipList) {
+        this.addView(currentChip)
+    }
 }
 
 
 @BindingAdapter("bindDetailFab", "bindDetailViewModel")
-fun FloatingActionButton.setDetailFab(article: Articles, viewModel: DetailViewModel) {
+fun FloatingActionButton.setDetailFab(
+    article: Articles,
+    viewModel: DetailViewModel
+) {
     this.setOnClickListener { view ->
         viewModel.insertFavorite(article)
         Snackbar.make(
@@ -82,15 +98,13 @@ fun FloatingActionButton.setDetailFab(article: Articles, viewModel: DetailViewMo
 }
 
 
-@BindingAdapter("bindDetailToolbar", "bindViewPager", "bindViewPagerFab")
+@BindingAdapter("bindViewPager", "bindViewPagerFab")
 fun Toolbar.setToolbar(
-    articles: Articles,
     viewPager: ViewPager2,
     viewPagerFab: FloatingActionButton
 ) {
     let { toolBar ->
 
-        toolBar.title = articles.title
         toolBar.setNavigationOnClickListener { view ->
             Navigation.findNavController(view).navigateUp()
         }
