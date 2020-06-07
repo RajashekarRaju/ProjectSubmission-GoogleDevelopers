b84544c2ae19d4f9b9ae02bbee305b89d2460570
rajas <rajashekarraju123@gmail.com>
2020-06-07 12:53:44 +0530

    Created custom MaterialAlertDialog for profile, shows only when internet available, added glide transformations

--- app/src/main/java/com/developersbreach/developersbreach/view/articles/ArticlesFragment.kt
+++ app/src/main/java/com/developersbreach/developersbreach/view/articles/ArticlesFragment.kt
@@ -1,17 +1,23 @@
 package com.developersbreach.developersbreach.view.articles
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.developersbreach.developersbreach.R
 import com.developersbreach.developersbreach.databinding.FragmentArticlesBinding
+import com.developersbreach.developersbreach.utils.isNetworkConnected
 import com.developersbreach.developersbreach.utils.showSnackBar
 import com.developersbreach.developersbreach.viewModel.ArticlesViewModel
 import com.developersbreach.developersbreach.viewModel.factory.ArticleViewModelFactory
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
 
 /**
  * A simple [Fragment] subclass.
@@ -35,6 +41,8 @@ class ArticlesFragment : Fragment() {
         binding = FragmentArticlesBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        ((activity) as AppCompatActivity).setSupportActionBar(binding.articlesToolbar)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -52,5 +60,59 @@ class ArticlesFragment : Fragment() {
             adapter.submitList(list)
             binding.articlesRecyclerView.adapter = adapter
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.profile_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.profile_menu_item -> {
+                if (isNetworkConnected(requireContext())) {
+                    showProfileDialog()
+                } else {
+                    showSnackBar(getString(R.string.no_internet_connection), requireActivity())
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showProfileDialog() {
+        val dialog: AlertDialog = createMaterialDialog()
+        setDialogViews(dialog)
+        dialog.show()
+    }
+
+    private fun createMaterialDialog(): AlertDialog {
+        return MaterialAlertDialogBuilder(context, R.style.MaterialDialog)
+            .setView(R.layout.profile_layout)
+            .create()
+    }
+
+    private fun setDialogViews(dialog: AlertDialog) {
+        dialog.setOnShowListener { alertDialog ->
+            alertDialog as AlertDialog
+            val imageView: ImageView = alertDialog.findViewById(R.id.profile_image_view)!!
+            val name: TextView = alertDialog.findViewById(R.id.profile_title)!!
+            val description: TextView = alertDialog.findViewById(R.id.profile_description)!!
+
+            with(viewModel) {
+                getAuthorDetails()
+                author.observe(viewLifecycleOwner, Observer { author ->
+                    name.text = author.name
+                    description.text = author.description
+                    Glide.with(requireContext())
+                        .load(author.avatar)
+                        .placeholder(R.drawable.ic_profile)
+                        .circleCrop()
+                        .into(imageView)
+                })
+            }
+        }
     }
 }
--- app/src/main/java/com/developersbreach/developersbreach/viewModel/ArticlesViewModel.kt
+++ app/src/main/java/com/developersbreach/developersbreach/viewModel/ArticlesViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.developersbreach.developersbreach.model.Articles
+import com.developersbreach.developersbreach.model.Author
 import com.developersbreach.developersbreach.repository.ArticlesRepository
 import com.developersbreach.developersbreach.repository.database.getDatabase
 import com.developersbreach.developersbreach.utils.isNetworkConnected
@@ -20,18 +21,24 @@ class ArticlesViewModel(application: Application) : AndroidViewModel(application
     private var viewModelJob = SupervisorJob()
     private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    val articles: LiveData<List<Articles>> = repository.articles
+    private val _articles: LiveData<List<Articles>> = repository.articles
+    val articles: LiveData<List<Articles>>
+        get() = _articles
 
     private val _isInternetAvailable = MutableLiveData<Boolean>()
     val isInternetAvailable: LiveData<Boolean>
         get() = _isInternetAvailable
 
+    private val _author = MutableLiveData<Author>()
+    val author: LiveData<Author>
+        get() = _author
+
     init {
-        refreshDataFromRepository()
+        refreshArticlesData()
         _isInternetAvailable.value = isNetworkConnected(application.applicationContext)
     }
 
-    private fun refreshDataFromRepository() {
+    private fun refreshArticlesData() {
         viewModelScope.launch {
             repository.refreshArticles()
         }
@@ -40,6 +47,13 @@ class ArticlesViewModel(application: Application) : AndroidViewModel(application
     fun insertFavorite(article: Articles) {
         viewModelScope.launch {
             repository.insertArticle(article)
+        }
+    }
+
+    fun getAuthorDetails() {
+        viewModelScope.launch {
+            val refreshAuthor = repository.getAuthor()
+            _author.postValue(refreshAuthor)
         }
     }
 
