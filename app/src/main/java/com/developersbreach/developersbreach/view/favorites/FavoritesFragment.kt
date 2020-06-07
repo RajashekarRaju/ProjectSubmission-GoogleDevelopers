7b536ee534bac01b3952cbc7bae2320f315606fc
rajas <rajashekarraju123@gmail.com>
2020-06-07 22:46:55 +0530

    Add equal spacing between favorite list items, Animate items in linear effect on resume

--- app/src/main/java/com/developersbreach/developersbreach/view/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/developersbreach/developersbreach/view/favorites/FavoritesFragment.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.developersbreach.developersbreach.databinding.FragmentFavoritesBinding
 import com.developersbreach.developersbreach.model.Articles
+import com.developersbreach.developersbreach.utils.RecyclerViewItemDecoration
+import com.developersbreach.developersbreach.utils.startLinearAnimation
 import com.developersbreach.developersbreach.viewModel.FavoritesViewModel
 import com.developersbreach.developersbreach.viewModel.factory.FavoritesViewModelFactory
 
@@ -38,6 +40,8 @@ class FavoritesFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        RecyclerViewItemDecoration.setItemSpacing(resources, binding.favoritesRecyclerView)
+
         viewModel.favorites.observe(viewLifecycleOwner, Observer { favoriteList ->
             val adapter = FavoritesAdapter(viewModel, this)
             adapter.submitList(favoriteList)
@@ -54,5 +58,14 @@ class FavoritesFragment : Fragment() {
             binding.favoritesRecyclerView.visibility = View.VISIBLE
             binding.noFavoritesFound.visibility = View.INVISIBLE
         }
+    }
+
+    /*
+     * Call animation from class [ArticleAnimations] and pass view which needs to animate.
+     * In our case we animate items of cardView inside RecyclerView.
+     */
+    override fun onResume() {
+        super.onResume()
+        startLinearAnimation(binding.favoritesRecyclerView)
     }
 }
